c1a89ecfa8a405fda985915d680d430b825235ea
experiment18a <119137845+ex18a@users.noreply.github.com>
2026-06-28 16:14:53 +0100

    Adjust parameter value ranges in gym.py

--- pwnagotchi/ai/gym.py
+++ pwnagotchi/ai/gym.py
@@ -12,13 +12,13 @@ class Environment(gym.Env):
     metadata = {'render.modes': ['human']}
     params = [
         Parameter('min_rssi', min_value=-200, max_value=-50),
-        Parameter('ap_ttl', min_value=30, max_value=600),
-        Parameter('sta_ttl', min_value=60, max_value=300),
+        Parameter('ap_ttl', min_value=15, max_value=600),
+        Parameter('sta_ttl', min_value=15, max_value=600),
 
-        Parameter('recon_time', min_value=5, max_value=60),
+        Parameter('recon_time', min_value=5, max_value=90),
         Parameter('max_inactive_scale', min_value=3, max_value=10),
-        Parameter('recon_inactive_multiplier', min_value=1, max_value=3),
-        Parameter('hop_recon_time', min_value=5, max_value=60),
+        Parameter('recon_inactive_multiplier', min_value=1, max_value=5),
+        Parameter('hop_recon_time', min_value=5, max_value=90),
         Parameter('min_recon_time', min_value=1, max_value=30),
         Parameter('max_interactions', min_value=1, max_value=25),
         Parameter('max_misses_for_recon', min_value=3, max_value=10),
@@ -143,4 +143,3 @@ class Environment(gym.Env):
             if 'histogram' in name:
                 logging.info("    %s" % name.replace('_histogram', ''))
                 self._render_histogram(value)
-
