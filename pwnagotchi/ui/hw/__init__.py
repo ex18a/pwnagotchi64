e2a651a8b15cd51a1cedf313e1e660eba5ee4de4
experiment18a <119137845+ex18a@users.noreply.github.com>
2026-07-02 02:11:21 +0100

    Add support for Waveshare V4 Portrait display

--- pwnagotchi/ui/hw/__init__.py
+++ pwnagotchi/ui/hw/__init__.py
@@ -8,6 +8,7 @@ from pwnagotchi.ui.hw.waveshare1 import WaveshareV1
 from pwnagotchi.ui.hw.waveshare2 import WaveshareV2
 from pwnagotchi.ui.hw.waveshare3 import WaveshareV3
 from pwnagotchi.ui.hw.waveshare4 import WaveshareV4
+from pwnagotchi.ui.hw.waveshare4portrait import WaveshareV4Portrait
 from pwnagotchi.ui.hw.waveshare27inch import Waveshare27inch
 from pwnagotchi.ui.hw.waveshare27inchv2 import Waveshare27inchV2
 from pwnagotchi.ui.hw.waveshare29inch import Waveshare29inch
@@ -53,6 +54,9 @@ def display_for(config):
     elif config['ui']['display']['type'] == 'waveshare_4':
         return WaveshareV4(config)
 
+    elif config['ui']['display']['type'] == 'waveshare_4_portrait':
+        return WaveshareV4Portrait(config)
+    
     elif config['ui']['display']['type'] == 'waveshare27inch':
         return Waveshare27inch(config)
 
