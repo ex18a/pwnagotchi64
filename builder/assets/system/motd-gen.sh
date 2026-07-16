3e0eb0005acf44e4a60bfec26c14252d4b3c6e1f
experiment18a <119137845+ex18a@users.noreply.github.com>
2026-07-16 07:36:21 +0100

    Revise motd-gen.sh with new configuration details
    Updated instructions for configuration and added a note about reporting issues.

--- builder/assets/system/motd-gen.sh
+++ builder/assets/system/motd-gen.sh
@@ -10,13 +10,14 @@ ${GREEN}        (◕‿‿◕) ${TARGET_HOSTNAME}
         Hi! I'm a pwnagotchi, please take good care of me!
         Here are some basic things you need to know to raise me properly!
 
-        If you want to change my configuration, use /etc/pwnagotchi/config.toml
-
-        All the configuration options can be found on /etc/pwnagotchi/default.toml,
-        but don't change this file because I will recreate it every time I'm restarted!
-
         for an easy guided setup instead of hand-editing the toml, use
         sudo pwnagotchi --setup
+
+        If you want to change my more advaanced configuration options, use 
+        sudo nano /etc/pwnagotchi/config.toml
+
+        All the default configuration options can be found in /etc/pwnagotchi/default.toml,
+        but don't change this file because I will recreate it every time I'm restarted!
 
         you can set up bluetooth connection, use sudo bt-wizard
 
@@ -25,14 +26,15 @@ ${GREEN}        (◕‿‿◕) ${TARGET_HOSTNAME}
         If you want to know what I'm doing, you can check my logs with the command
         tail -f /var/log/pwnagotchi.log
 
+        watch what im doing in real time with pwnlog
+
         If you want to know if I'm running, you can use
         systemctl status pwnagotchi
 
         You can restart me using
         systemctl restart pwnagotchi
 
-        But be aware I will go into MANUAL mode when restarted!
-        You can put me back into AUTO mode using
-        touch /root/.pwnagotchi-auto && systemctl restart pwnagotchi
+        if you find and bugs or having trouble with anything please raise an issue at
+        github.com/ex18a/pwnagotchi64
         ${NC}
 MOTD_EOF
