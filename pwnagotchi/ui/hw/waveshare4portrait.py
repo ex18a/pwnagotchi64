c99ad1d4721ded01c16ab5572dca736657b0a1a9
ex18a <experiment18a@gmail.com>
2026-09-09 13:57:39 +0100

    Make the e-ink border waveform theme-aware so it follows the black/white display color setting

--- pwnagotchi/ui/hw/waveshare4portrait.py
+++ pwnagotchi/ui/hw/waveshare4portrait.py
@@ -59,6 +59,9 @@ class WaveshareV4Portrait(DisplayImpl):
         logging.info("initializing waveshare v4 portrait driver")
         from pwnagotchi.ui.hw.libs.waveshare.v4.epd2in13_V4 import EPD
         self._display = EPD()
+        if self.bg_color == 0x00:
+            self._display.border_full = 0x40
+            self._display.border_partial = 0x40
         self._display.init()
         self._display.Clear(self.bg_color)
         logging.info("initializing waveshare v4 portrait driver done")
